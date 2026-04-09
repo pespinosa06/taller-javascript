a72f251e583a931b3ccf44fb6555d5182cc8975a
pespinosa06 <pespinosar@unbosque.edu.co>
2026-04-09 08:30:57 -0500

    commit declaracion de variables

---
+++ index.js
@@ -0,0 +1,9 @@
+let nombre = ""
+let edad = 0
+let tipo_de_documento = "" 
+let numero_de_documento = 0
+let salario = 0
+let comisiones = 0
+let Total_de_horas_extras = 0
+let calificacion_de_nivel_de_riesgo = ""
+
