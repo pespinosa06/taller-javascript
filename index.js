4f2628dcb7147e28876fa59ad0814a6cf9300989
pespinosa06 <pespinosar@unbosque.edu.co>
2026-05-16 08:22:53 -0500

    index.js cambio

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let nombre = document.getElementById("nombre") ;
-let edad = document.getElementById("edad") ;
-let tipo_de_documento = document.getElementById("tipo_de_documento") ;
-let numero_de_documento = 0 ;
+let nombre = document.getElementById("nombre").value ;
+let edad = document.getElementById("edad") .value ;
+let tipo_de_documento = document.getElementById("tipo_de_documento").value ;
+let numero_de_documento = document.getElementById("numero_de_documento").value ;
 
 if (edad < 18) {
     console.log("No puede entrar, es menor de edad") ;
@@ -26,40 +26,33 @@ let Total_de_horas_extras = 0 ;
 let calificacion_de_nivel_de_riesgo = "" ;
 
 
-  const SMLV = 1750905 ;
-  const SMIV = 22761765 ;
-  const ST = 249095 ;
-  const UDVT = 52.37 ;
+const SMLV = 1750905 ;
+const SMIV = 22761765 ;
+const ST = 249095 ;
+const UDVT = 52.37 ;
 
-  const Riesgo_1 = 0.522 ;
-  const Riesgo_2 = 1.044 ;
-  const Riesgo_3 = 2.436 ;
-  const Riesgo_4 = 4.350 ;
-  const Riesgo_5 = 6.960 ;
-  const riesgo = [Riesgo_1, Riesgo_2, Riesgo_3, Riesgo_4, Riesgo_5] ;
+const Riesgo_1 = 0.522 ;
+const Riesgo_2 = 1.044 ;
+const Riesgo_3 = 2.436 ;
+const Riesgo_4 = 4.350 ;
+const Riesgo_5 = 6.960 ;
+const riesgo = [Riesgo_1, Riesgo_2, Riesgo_3, Riesgo_4, Riesgo_5] ;
 
-  const porcentajeIBC = 0.7 ;
-  const porcentajeSalud = 0.04 ;
-  const porcentajePension = 0.04 ;
+const porcentajeIBC = 0.7 ;
+const porcentajeSalud = 0.04 ;
+const porcentajePension = 0.04 ;
 
   function Calcularporcentaje (base, porcentaje) {
     return base * porcentaje ;
   }
  
-
-
-
   let salario_Total = salario + comisiones + horas_extras;
   let IBC = salario_Total * porcentajeIBC ;
   let salud = IBC * porsentajeSalud ;
   let pension = IBC * porcentajePension ;
 
   // aqui se agrega la validacion de datos y el if para mostrar el error y hacer el return de la pagina :3
-  function validacionDatos () 
-  nombre = document.getElementById("nombre").value ;
-  edad = document.getElementById("edad") .value ;
-  tipo_de_documento = document.getElementById("tipo_de_documento").value ;
-  numero_de_documento = document.getElementById("numero_de_documento").value ;
+  function validacionDatos(){ 
   if(!nombre)
     mostrarError("El campo nombre es obligatorio") ;
 return;
@@ -72,4 +65,4 @@ return;
     if(!numero_de_documento)
     mostrarError("El campo numero de documento es obligatorio") ;
 return;
-  
+}
